c44065022295b2ca28993db14b0ee15161e77143
hejny <me@pavolhejny.com>
2017-03-19 01:10:17 +0100

    [PH] adding items by Enter

--- src/script/components/todo-create.tsx
+++ src/script/components/todo-create.tsx
@@ -6,9 +6,18 @@ export function TodoCreate(props) {
     const {state, dispatch} = props;
 
 
+
+    const handleKeyPress = (event) => {
+        if(event.key == 'Enter'){
+            dispatch({type:'COMMIT_CURRENT_TODO'});
+        }
+    };
+
+
+
     return (
         <div>
-            <input type="text" onChange={(event)=>dispatch({type:'CHANGE_CURRENT_TODO',todo:event.target.value})} value={state.current_todo} />
+            <input type="text" onChange={(event)=>dispatch({type:'CHANGE_CURRENT_TODO',todo:event.target.value})} value={state.current_todo} onKeyPress={handleKeyPress} />
             <button onClick={dispatch.bind(this,{type:'COMMIT_CURRENT_TODO'})}>Create</button>
         </div>
     );
