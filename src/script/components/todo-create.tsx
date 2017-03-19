36e27bcdd9c2e0fe4d35e683b3b039911685d41c
hejny <me@pavolhejny.com>
2017-03-19 02:27:54 +0100

    [PH] state persisted in browser history - debounced

--- src/script/components/todo-create.tsx
+++ src/script/components/todo-create.tsx
@@ -1,7 +1,9 @@
 import * as React from "react";
 
 
+
 export function TodoCreate(props) {
+
 
     const {state, dispatch} = props;
 
@@ -9,15 +11,30 @@ export function TodoCreate(props) {
 
     const handleKeyPress = (event) => {
         if(event.key == 'Enter'){
+
             dispatch({type:'COMMIT_CURRENT_TODO'});
+
         }
     };
+
+
+    const handleChange = (event) => {
+        dispatch({type:'CHANGE_CURRENT_TODO_NAME',todo: event.target.value})
+    };
+
 
 
 
     return (
         <div>
-            <input type="text" onChange={(event)=>dispatch({type:'CHANGE_CURRENT_TODO_NAME',todo: event.target.value})} value={state.current_todo.name} onKeyPress={handleKeyPress} />
+            <input
+                type="text"
+                value={state.current_todo.name}
+                onChange={handleChange}
+                onKeyPress={handleKeyPress}
+
+
+            />
             <button onClick={dispatch.bind(this,{type:'COMMIT_CURRENT_TODO'})}>Create</button>
         </div>
     );
--- src/script/functions/create-history-reducer.ts
+++ src/script/functions/create-history-reducer.ts
@@ -1,16 +1,33 @@
 
-
+import * as _ from "lodash";
 
 
 export function createHistoryReducer(reducer,){
+
+
+
+    let stateWaitingForPush;
+
+
+
+    const debouncedPushState = _.debounce(()=>{
+
+        history.pushState(stateWaitingForPush.toJS(), '', '#');
+
+    },500);
+
+
+
 
     return function(state,action){
 
 
         const newState = reducer(state,action);
+        stateWaitingForPush = newState;
 
         if(action.type!=='CHANGE_STATE' && action.type!=='@@redux/INIT'){
-            history.pushState(newState.toJS(), '', '#');
+
+            debouncedPushState();
 
         }
 
